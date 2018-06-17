68af56ea3ea57d63399e245ad9931f83c710e047
Marcelo de Moraes <marcelodemoraes11@gmail.com>
2018-06-16 22:48:06 -0300

    Classe para conexao e juntei Menu + TrabalhoBD

---
+++ trabalhoBD/src/trabalhobd/Connect.java
@@ -0,0 +1,64 @@
+package trabalhobd;
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ * 
+ * @author Marcelo de Moraes
+ *
+ */
+
+//Estabelece conexao entre usuario e banco de dados
+
+public class Connect {
+	private String url;
+	private String user;
+	private String pass;
+	private Connection c;
+	private Statement stmt; 
+	
+	//construtores
+	public Connect() { //deafult
+        this.url = "jdbc:oracle:thin:@grad.icmc.usp.br:15215:orcl";
+        this.user = "M9763151";
+        this.pass = "mk21011998*";	     
+	}
+	
+	public Connect(String user, String pass) {
+		this.user = user;
+		this.pass = pass;
+	}
+	
+	public Connect(String user, String pass, String url) {
+		this.user = user;
+		this.pass = pass;
+		this.url = url;
+	}
+	
+	public Statement makeConnection() {
+		
+		try {
+			c = DriverManager.getConnection(url, user, pass);
+			stmt = c.createStatement();
+		}
+		catch(SQLException sqle){
+			sqle.printStackTrace();
+			stmt = null;
+		}
+		
+		return stmt;	
+	}
+	
+	public void close() {
+		try {
+			c.close();
+			stmt.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
